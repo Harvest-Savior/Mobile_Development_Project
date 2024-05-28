6aaf5d4e21b42efd93970352ac51c6e725c14be5
Afdholanabil <afdholanabil41435@gmail.com>
2024-05-28 21:38:11 +0700

    initial commit

---
+++ app/src/main/java/com/example/harvest_savior_mobile_dev/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.harvest_savior_mobile_dev
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
